4fdc409c4437fc962122ad684fc8c1af296b3c62
David Hybner <dah1998@yahoo.com>
2022-11-17 15:07:47 -0500

    install and update server.js
    changed mongoose URI to work on local host port. added new routes as well

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -39,7 +39,114 @@ const userController = {
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(400).json(err));
   },
+// updates user
+ updateUser({ params, body }, res) {
+  User.findOneAndUpdate({ _id: params.id }, body, {
+    new: true,
+    runValidators: true,
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => res.status(400).json(err));
+},
 
+//delete user by id 
+deleteUser({ params }, res) {
+  // delete the user
+  User.findOneAndDelete({ _id: params.id })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      // update friends lists to reflect deleted user
+      User.updateMany(
+        { _id: { $in: dbUserData.friends } },
+        { $pull: { friends: params.id } }
+      )
+        .then(() => {
+          // removes all posts by this user 
+          Thought.deleteMany({ username: dbUserData.username })
+            .then(() => {
+              res.json({ message: 'Successfully deleted user: '+username });
+            })
+            .catch((err) => res.status(400).json(err));
+        })
+        .catch((err) => res.status(400).json(err));
+    })
+    .catch((err) => res.status(400).json(err));
+},
+
+// add friend by friend id 
+addFriend({ params }, res) {
+ 
+  User.findOneAndUpdate(
+    { _id: params.userId },
+    { $addToSet: { friends: params.friendId } },
+    { new: true, runValidators: true }
+  )
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this userId' });
+        return;
+      }
+      // adds friends id to the users id to keep it specific to the user
+      User.findOneAndUpdate(
+        { _id: params.friendId },
+        { $addToSet: { friends: params.userId } },
+        { new: true, runValidators: true }
+      )
+        .then((dbUserData2) => {
+          if (!dbUserData2) {
+            res
+              .status(404)
+              .json({ message: 'No user found with this friendId' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch((err) => res.json(err));
+    })
+    .catch((err) => res.json(err));
+},
+
+
+deleteFriend({ params }, res) {
+  // removes friend 
+  User.findOneAndUpdate(
+    { _id: params.userId },
+    { $pull: { friends: params.friendId } },
+    { new: true, runValidators: true }
+  )
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this userId' });
+        return;
+      }
+      // remove userId from friendId's friend list
+      User.findOneAndUpdate(
+        { _id: params.friendId },
+        { $pull: { friends: params.userId } },
+        { new: true, runValidators: true }
+      )
+        .then((dbUserData2) => {
+          if (!dbUserData2) {
+            res
+              .status(404)
+              .json({ message: 'No user found with this friendId' });
+            return;
+          }
+          res.json({ message: 'Successfully deleted the friend' });
+        })
+        .catch((err) => res.json(err));
+    })
+    .catch((err) => res.json(err));
+},
  
 
  };
